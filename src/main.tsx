94e8dc6bacc032f051b78aec2bfc6fb4abd5881a
ryouhama <r0730hacap@icloud.com>
2024-01-27 20:49:00 +0900

    First commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { PublicRouter } from "~/router"
-
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PublicRouter />
+    <App />
   </React.StrictMode>,
 )
--- src/router/index.ts
+++
@@ -1,1 +0,0 @@
-export { PublicRouter } from './public'
